b3d4b90bcca7edfee2f0e75583afde8897fc199e
JackAnderson01 <hammad.haq@launchbox.pk>
2024-02-11 15:14:51 +0500

    Created UserManager for user creation

--- src/demo/managers.py
+++ src/demo/managers.py
@@ -6,4 +6,29 @@ class UserManager(BaseUserManager):
     
 
     def create_user(self, first_name, last_name, email, phone_number, password=None):
-        pass
+        
+        if not first_name:
+            raise ValueError("First Name is required.")
+        elif not last_name:
+            raise ValueError("Last Name is required.")
+        elif not email:
+            raise ValueError("Email is required.")
+        elif not phone_number:
+            raise ValueError("Phone number is required.")
+
+        user = self.model(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+
+    def create_superuser(self, first_name, last_name, email, phone_number, password):
+        user = self.create_user(first_name, last_name, email, phone_number, password)
+
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(self._db)
+
+        return user
